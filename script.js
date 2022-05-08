c77d9ba5967cd55ec1abff980d2235e4dd38b2d1
tgrthelegit <105061853+tgrthelegit@users.noreply.github.com>
2022-05-08 08:54:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("Welcome to Tic Tac Toe")
-
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
